c6efb2bede20124ed6b2944bd7def6c56c8d5f04
Pavel Matveev <reus1896@gmail.com>
2022-09-05 13:02:44 +0300

    add 1 path

---
+++ src/Aviasales.java
@@ -0,0 +1,7 @@
+public class Aviasales {
+    public static void main(String[] args) {
+        int x = 34_499;    // стоимость билета
+        int y = 20;       // стоимость бонусной мили
+        System.out.println((x / y) + " бонусных миль за покупку билета");
+    }
+}
